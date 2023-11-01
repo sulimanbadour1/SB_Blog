a4b9cb5a27a752999b0f47e36a8e113322ab551c
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-11-02 00:37:15 +0100

    added categoryList

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,74 +1,77 @@
-import Link from "next/link";
-import styles from "./CategoryList.module.css";
 import Image from "next/image";
+import styles from "./CategoryList.module.css";
+import Link from "next/link";
 
 const CategoryList = () => {
   return (
-    <div>
-      <h1 className={styles.title}>Popular Categories</h1>
+    <div className={styles.container}>
+      <h2 className={styles.title}>Featured Categories</h2>
       <div className={styles.categories}>
-        <div className={styles.category}>
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles.travel}`}
-          >
-            <Image
-              src="/travel.svg"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Travel
-          </Link>
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles.it}`}
-          >
-            <Image
-              src="/code.svg"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            IT
-          </Link>
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles.academia}`}
-          >
-            <Image
-              src="/academia.svg"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Academia
-          </Link>
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles.engineering}`}
-          >
-            <Image
-              src="/engineering.svg"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Engineering
-          </Link>
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles.art}`}
-          >
-            <Image
-              src="/camera.svg"
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            Art
-          </Link>
-        </div>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/code.svg"
+            width={32}
+            height={32}
+            alt="category"
+            className={styles.image}
+          />
+          Coding
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/camera.svg"
+            width={32}
+            height={32}
+            alt="category"
+            className={styles.image}
+          />
+          Art
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/engineering.svg"
+            width={32}
+            height={32}
+            alt="category"
+            className={styles.image}
+          />
+          Engineering
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/travel.svg"
+            width={32}
+            height={32}
+            alt="category"
+            className={styles.image}
+          />
+          Travel
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/academia.svg"
+            width={32}
+            height={32}
+            alt="category"
+            className={styles.image}
+          />
+          Science
+        </Link>
       </div>
     </div>
   );
