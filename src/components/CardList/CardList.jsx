93aabc46f0735d1cb25cf139fa8c410ba83981f1
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-11-02 01:08:05 +0100

    added cards

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import Pagination from "../Pagination/Pagination";
 import styles from "./CardList.module.css";
+import Card from "../card/Card";
 
 const CardList = () => {
   return (
     <header className={styles.container}>
-      CardList
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <div className={styles.post}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
       <Pagination />
     </header>
   );
---
+++ src/components/card/Card.jsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import styles from "./Card.module.css";
+import Link from "next/link";
+
+const Card = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/karl.webp" alt="post" fill className={styles.image} />
+      </div>
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>2.11.2023 -</span>
+          <span className={styles.category}>Travel</span>
+        </div>
+        <Link href="/post">
+          <h1>Title</h1>
+        </Link>
+
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident,
+          odio nulla at qui vero rerum possimus odit tempore magni vel eius
+          quas! Unde facere hic officiis, vel ipsa amet commodi.
+        </p>
+        <Link href="/post" className={styles.link}>
+          Read More
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
