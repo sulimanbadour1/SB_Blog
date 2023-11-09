56107d2bfd811fc1b57d22f0fe402b7e2a054f83
Suliman Badour <71437804+sulimanbadour1@users.noreply.github.com>
2023-11-09 19:32:21 +0100

    Update ESLint configuration and remove unused variable

--- src/app/api/categories/route.js
+++ src/app/api/categories/route.js
@@ -1,6 +1,6 @@
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
+
 export const GET = async () => {
   try {
     const categories = await prisma.category.findMany();
